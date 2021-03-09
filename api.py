291efd4102e0c550d00e66d8361426a3948594cc
matheu55391 <matheus.manulo55@gmail.com>
2021-03-09 14:15:01 -0300

    Criado teste de unidade, criado formulario(), criado pingpong()

--- api.py
+++ api.py
@@ -1,17 +1,39 @@
 import requests
 def getUSD():
+    #retorna o valor do dolar
     dolar = requests.get('https://economia.awesomeapi.com.br/json/usd').json()
     return float(dolar[0]['bid'])
 
+def formulario(pergunta , func, resposta):
+    #:) uma brincadeirinha para gerar o formulario sem poluir o codigo
+    num = float(input(pergunta))
+    #numero de saida     #numero de entrada
+    num2 = func(num)
+    resposta = resposta.replace('FUNCnum1', f'{num:.2f}')
+    resposta = resposta.replace('FUNCnum2', f'{num2:.2f}')
+    print(resposta)
+    
+def testeUnidade_valorNumerico(num):
+    #teste de unidade
+    try:        
+        return float(num)
+    except:
+        return ValueError(f'Valor {num} não é um número.')
+
+
 def realParaDolar(real):
-    return real / getUSD()
+    #converte de real para dolar
+    return testeUnidade_valorNumerico(real) / getUSD()
 
 def dolarParaReal(dolar):
-    return dolar * getUSD()
+    #converte de dolar para real
+    return testeUnidade_valorNumerico(dolar) * getUSD()
 
 
+def pingpong(num):
+    return testeUnidade_valorNumerico(num)
+
+#print(pingpong('a'))
 
 
-
-
-
+        
--- main.py
+++ main.py
@@ -4,21 +4,25 @@ def main():
     while True:
         print('\n'*100)
         print('----------- Cambio ----------')
-        opc = int(input('Escolha uma opção \n1 - Real para Dolar\n2 - Dolar para Real\n0 - Sair\n: '))
-        if opc == 1:
-            real = float(input('Quantos Reais você deseja converter em dolar ? '))
-            dolar = realParaDolar(real)
-            print(f'R${real} Equivalem a U${dolar}')
+        print(f'      U${getUSD()} -> R$ 1.00\n') 
+        try:
+            opc = int(input('Escolha uma opção \n1 - Real para Dolar\n2 - Dolar para Real\n0 - Sair\n: '))
             
-        elif opc == 2:
-            dolar = float(input('Quantos Dolares você deseja converter em Reais ? '))
-            real = dolarParaReal(dolar)
-            print(f'U${dolar} Equivalem a R${real}')
-            
-        elif opc == 0:
-            break
-        else:
-            print('Opção Invalida, tente novamente\n')
+            if opc == 1:
+
+                formulario('Quantos Reais você deseja converter em dolar ?\n:', realParaDolar, f'R$ FUNCnum1 equivalem a U$ FUNCnum2')
+                
+            elif opc == 2:
+                formulario('Quantos Dolares você deseja converter em Reais ?\n:', dolarParaReal, f'U$ FUNCnum1 equivalem a R$ FUNCnum2')
+                
+            elif opc == 0:
+                break
+            else:
+                print('Opção Invalida, tente novamente\n')
+        except:
+            print("Input invalido!!!\nApenas nÚmeros são aceitos.")
         input('...')
 
-main()
+
+if __name__ == "__main__":
+    main()
